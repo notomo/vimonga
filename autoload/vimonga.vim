3e34bb6d3fc4a9c90a87c8c1435713b62d8af176
notomo <notomo.motono@gmail.com>
2019-02-05 09:13:04 +0900

    Add host, port options

--- autoload/vimonga.vim
+++ autoload/vimonga.vim
@@ -1,6 +1,10 @@
 
 function! vimonga#start() abort
-    let db_names = systemlist('vimonga')
+    let host = shellescape(vimonga#config#get('default_host'))
+    let port = vimonga#config#get('default_port')
+    let cmd = join(['vimonga', '-h', host, '-p', port], ' ')
+
+    let db_names = systemlist(cmd)
 
     tabnew
     setlocal buftype=nofile
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,30 @@
 extern crate mongodb;
 use mongodb::{Client, ThreadedClient};
 
+extern crate getopts;
+use getopts::Options;
+
+use std::env;
+
 fn main() {
-    let client = Client::connect("localhost", 27020).expect("Failed to initialize client.");
+    let args: Vec<String> = env::args().collect();
+    let mut options = Options::new();
+    options.optopt("h", "host", "host string", "");
+    options.optopt("p", "port", "port number", "");
+
+    let matches = match options.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => panic!(f.to_string()),
+    };
+
+    let host = matches.opt_str("h").unwrap_or("localhost".to_string());
+    let port = matches
+        .opt_str("p")
+        .unwrap_or("27020".to_string())
+        .parse()
+        .unwrap();
+
+    let client = Client::connect(&host, port).expect("Failed to initialize client.");
 
     let names = client
         .database_names()

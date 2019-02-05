8375d5f52ee728a074235d3a53ce4c4766ecdfd8
notomo <notomo.motono@gmail.com>
2019-02-05 21:08:52 +0900

    Add get_collection_names()

--- autoload/vimonga.vim
+++ autoload/vimonga.vim
@@ -1,15 +1,32 @@
 
-function! vimonga#start() abort
-    let host = shellescape(vimonga#config#get('default_host'))
-    let port = vimonga#config#get('default_port')
-    let cmd = join(['vimonga', '-h', host, '-p', port], ' ')
+function! vimonga#databases() abort
+    let db_names = s:execute([])
 
-    let db_names = systemlist(cmd)
+    call s:buffer(db_names)
+endfunction
 
+function! vimonga#collections(database_name) abort
+    let name = shellescape(a:database_name)
+    let collection_names = s:execute(['-d', name, '-m', 'collection'])
+
+    call s:buffer(collection_names)
+endfunction
+
+function! s:buffer(contents) abort
     tabnew
+
     setlocal buftype=nofile
     setlocal nobuflisted
     setlocal noswapfile
 
-    call setline(1, db_names)
+    call setline(1, a:contents)
+endfunction
+
+function! s:execute(args) abort
+    let host = shellescape(vimonga#config#get('default_host'))
+    let port = vimonga#config#get('default_port')
+    let default_args = ['vimonga', '-h', host, '-p', port]
+
+    let cmd = join(default_args + a:args, ' ')
+    return systemlist(cmd)
 endfunction
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate mongodb;
-use mongodb::{Client, ThreadedClient};
+use mongodb::db::ThreadedDatabase;
+use mongodb::{doc, Client, ThreadedClient};
 
 extern crate getopts;
 use getopts::Options;
@@ -11,6 +12,8 @@ fn main() {
     let mut options = Options::new();
     options.optopt("h", "host", "host string", "");
     options.optopt("p", "port", "port number", "");
+    options.optopt("m", "method", "method name", "");
+    options.optopt("d", "dbname", "database name", "");
 
     let matches = match options.parse(&args[1..]) {
         Ok(m) => m,
@@ -24,13 +27,33 @@ fn main() {
         .parse()
         .unwrap();
 
+    let database_name = matches.opt_str("d").unwrap_or("".to_string());
+
     let client = Client::connect(&host, port).expect("Failed to initialize client.");
 
-    let names = client
+    let names = match matches.opt_str("m").unwrap_or("".to_string()).as_str() {
+        "db" => get_database_names(&client),
+        "collection" => get_collection_names(&client, database_name.as_str()),
+        _ => get_database_names(&client),
+    };
+
+    let joined = names.join("\n");
+    println!("{}", joined);
+}
+
+fn get_database_names(client: &Client) -> Vec<String> {
+    client
         .database_names()
         .ok()
-        .expect("Failed to get database names");
-    let joined = names.join("\n");
+        .expect("Failed to get database names")
+}
 
-    println!("{}", joined);
+fn get_collection_names(client: &Client, database_name: &str) -> Vec<String> {
+    let filter = Some(doc! {});
+
+    client
+        .db(database_name)
+        .collection_names(filter)
+        .ok()
+        .expect("Failed to get collection names")
 }

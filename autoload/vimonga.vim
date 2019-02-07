43ddfed3af89cd2a1dd59f9d0050799ab5827f5c
notomo <notomo.motono@gmail.com>
2019-02-08 08:23:54 +0900

    Add find with query and projection

--- autoload/vimonga.vim
+++ autoload/vimonga.vim
@@ -12,10 +12,20 @@ function! vimonga#collections(database_name) abort
     call s:buffer(collection_names)
 endfunction
 
-function! vimonga#documents(database_name, collection_name) abort
+function! vimonga#documents(database_name, collection_name, ...) abort
     let database_name = shellescape(a:database_name)
     let collection_name = shellescape(a:collection_name)
-    let documents = s:execute(['-d', database_name, '-c', collection_name, '-m', 'document'])
+    let args = ['-d', database_name, '-c', collection_name, '-m', 'document']
+    if len(a:000) >= 1 && len(a:000[0]) > 0
+        let query = shellescape(a:000[0])
+        call extend(args, ['-q', query])
+    endif
+    if len(a:000) >= 2  && len(a:000[1]) > 0
+        let projection = shellescape(a:000[1])
+        call extend(args, ['-r', projection])
+    endif
+
+    let documents = s:execute(args)
 
     call s:buffer(documents)
 endfunction
@@ -33,7 +43,7 @@ endfunction
 function! s:execute(args) abort
     let host = shellescape(vimonga#config#get('default_host'))
     let port = vimonga#config#get('default_port')
-    let default_args = ['vimonga', '-h', host, '-p', port]
+    let default_args = ['RUST_BACKTRACE=1', 'vimonga', '-h', host, '-p', port]
 
     let cmd = join(default_args + a:args, ' ')
     return systemlist(cmd)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+extern crate bson;
+use bson::Document;
+
 extern crate mongodb;
 use mongodb::coll::options::FindOptions;
 use mongodb::db::ThreadedDatabase;
@@ -7,6 +10,9 @@ extern crate getopts;
 use getopts::Options;
 
 extern crate serde_json;
+use serde_json::Value;
+
+use std::collections::HashMap;
 
 use std::env;
 
@@ -18,6 +24,8 @@ fn main() {
     options.optopt("m", "method", "method name", "");
     options.optopt("d", "db-name", "database name", "");
     options.optopt("c", "collection-name", "collection name", "");
+    options.optopt("q", "query", "find query", "");
+    options.optopt("r", "projection", "find projection", "");
 
     let matches = match options.parse(&args[1..]) {
         Ok(m) => m,
@@ -33,13 +41,21 @@ fn main() {
 
     let database_name = matches.opt_str("d").unwrap_or("".to_string());
     let collection_name = matches.opt_str("c").unwrap_or("".to_string());
+    let query = matches.opt_str("q").unwrap_or("{}".to_string());
+    let projection = matches.opt_str("r").unwrap_or("{}".to_string());
 
     let client = Client::connect(&host, port).expect("Failed to initialize client.");
 
     let content = match matches.opt_str("m").unwrap_or("".to_string()).as_str() {
         "db" => get_database_names(&client),
         "collection" => get_collection_names(&client, database_name.as_str()),
-        "document" => get_documents(&client, database_name.as_str(), collection_name.as_str()),
+        "document" => get_documents(
+            &client,
+            database_name.as_str(),
+            collection_name.as_str(),
+            query.as_str(),
+            projection.as_str(),
+        ),
         _ => get_database_names(&client),
     };
 
@@ -65,20 +81,38 @@ fn get_collection_names(client: &Client, database_name: &str) -> String {
         .join("\n")
 }
 
-fn get_documents(client: &Client, database_name: &str, collection_name: &str) -> String {
-    let filter = Some(doc! {});
+fn get_documents(
+    client: &Client,
+    database_name: &str,
+    collection_name: &str,
+    query_json: &str,
+    projection_json: &str,
+) -> String {
+    let query = Some(to_document_from_str(query_json));
+    let projection = Some(to_document_from_str(projection_json));
 
     let mut find_option = FindOptions::new();
     find_option.limit = Some(10);
+    find_option.projection = projection;
 
     let cursor = client
         .db(database_name)
         .collection(collection_name)
-        .find(filter, Some(find_option))
+        .find(query, Some(find_option))
         .ok()
         .expect("Failed to get documents");
 
     let documents: Vec<_> = cursor.map(|doc| doc.unwrap()).collect();
 
     serde_json::to_string_pretty(&documents).unwrap()
+}
+
+fn to_document_from_str(json_str: &str) -> Document {
+    let json_value: HashMap<String, Value> = serde_json::from_str(json_str).unwrap();
+
+    let mut document = Document::new();
+    for (key, value) in json_value {
+        document.insert_bson(key.to_string(), value.into());
+    }
+    document
 }
